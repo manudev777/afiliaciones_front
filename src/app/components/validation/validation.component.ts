b8d9f6815ee6ea4c321a5af138354e3d727f227f
percypinaud <percypinaud@upeu.edu.pe>
2025-01-22 07:05:13 -0500

    add: avance del formulario de validacion

--- src/app/components/validation/validation.component.ts
+++ src/app/components/validation/validation.component.ts
@@ -1,18 +1,40 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../shared/navbar/navbar.component';
-import { AuthService } from '../../services/auth.service';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-validation',
   imports: [
     CommonModule,
     NavbarComponent,
+    ReactiveFormsModule
   ],
   standalone:true,
   templateUrl: './validation.component.html',
-  providers:[AuthService]
+  providers:[]
 })
 export class ValidationComponent {
+    form: FormGroup;
 
+    constructor(private fb: FormBuilder) {
+      this.form = this.fb.group({
+        tipoPersona: ['natural', Validators.required],
+        ruc: ['', [Validators.required, Validators.pattern(/^\d{11}$/)]],
+        dni: ['', [Validators.required, Validators.pattern(/^\d{8}$/)]], 
+        razonSocial: [''],
+        direccionComercial: [false],
+        tipoVia: ['calle', Validators.required],
+        nombreVia: ['', Validators.required],
+        regimen: ['rg', Validators.required],
+      });
+    }
+
+    onSubmit(): void {
+      if (this.form.valid) {
+        console.log('Formulario enviado:', this.form.value);
+      } else {
+        console.log('Formulario inválido.');
+      }
+    }
 }
